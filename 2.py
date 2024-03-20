4e644387d7ba7c5e8434752158b6b84f57989448
Amato de Lusarreta, Franco <famatodelusarreta@udesa.edu.ar>
2024-03-20 15:42:47 -0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,37 @@
-with open("/Users/franco/Documentos/UdeSA/2do año/1er cuatrimestre/Metodos numericos/MNyO_TP01_2024_1S/mnyo_mediciones.csv", 'r') as file:
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import lagrange, CubicSpline
+
+x = []
+y = []
+
+with open("mnyo_mediciones.csv", 'r') as file:
     data = file.read()
-    print(data)
+
+    data = data.split("\n")
+    data.remove(data[-1])
+
+    for i in range(len(data)):
+        data[i] = data[i].split(" ")
+
+    for i in range(len(data)):
+        for ii in range(len(data[i])):
+            data[i][ii] = data[i][ii].split("e")
+
+    for i in range(len(data)):
+        for ii in range(len(data[i])):
+            data[i][ii][0] = float(data[i][ii][0])
+            data[i][ii][1] = int(data[i][ii][1])
+
+    for i in range(len(data)):
+        x.append(data[i][0][0] * (10 ** data[i][0][1]))
+        y.append(data[i][1][0] * (10 ** data[i][1][1]))
+
+# spline = CubicSpline(x, y)
+# y_spline = spline(x)
+
+plt.plot(x, y, label="Original")
+# plt.plot(x, y_spline, label="Spline")
+plt.legend()
+plt.grid()
+plt.show()
