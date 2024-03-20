ba6f84824ac646a7d6d44197abf4ceb5aca31f1c
totobena28 <benavideztomas28@gmail.com>
2024-03-20 16:32:12 -0300

    primera parte del 2
    ya se filtro la info del csv, falta armar trayectoria

--- 1_b.py
+++ 1_b.py
@@ -1,120 +1,125 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.interpolate import lagrange, CubicSpline
-import numpy.polynomial.chebyshev
-from numpy import exp
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from scipy.interpolate import lagrange, CubicSpline
+# import numpy.polynomial.chebyshev
+# from numpy import linspace
+# from numpy import meshgrid
+# import matplotlib.pyplot as plt
+# from scipy.interpolate import lagrange, CubicSpline
+# from numpy import exp
 
-#tengo que analizar lagrange y splines en una funcion de dos variables
-def FrankeFunction(x,y):
-    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-    return term1 + term2 + term3 + term4
+# #tengo que analizar lagrange y splines en una funcion de dos variables
+# def FrankeFunction(x,y):
+#     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+#     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+#     term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+#     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+#     return term1 + term2 + term3 + term4
 
-x = np.linspace(0, 1, 100)
-y = np.linspace(0, 1, 100)
-n_points = 100
-# Generar puntos para graficar la función de Franke
-x, y = np.meshgrid(x, y)
-z = FrankeFunction(x, y)
+# x = linspace(0, 1, 100)
+# y = linspace(0, 1, 100)
+# n_points = 100
+# # Generar puntos para graficar la función de Franke
+# x, y = np.meshgrid(x, y)
+# z = FrankeFunction(x, y)
 
-# Puntos de interpolación para la funcion de Franke
-x_interpolation = np.random.rand(n_points)
-y_interpolation = np.random.rand(n_points)
-z_interpolation = FrankeFunction(x_interpolation, y_interpolation)
+# # Puntos de interpolación para la funcion de Franke
+# x_interpolation = np.random.rand(n_points)
+# y_interpolation = np.random.rand(n_points)
+# z_interpolation = FrankeFunction(x_interpolation, y_interpolation)
 
-# Interpolación de Lagrange para la función de Franke
-lagrange_poly = lagrange((x_interpolation, y_interpolation), z_interpolation)
-z_lagrange = lagrange_poly((x, y))
-# Polinomios cúbicos splines para la función de Franke
-spline = CubicSpline((x_interpolation, y_interpolation), z_interpolation)
-z_spline = spline((x, y))
-
-
-# Polinomios de Chebyshev para la función de Franke
-def chebyshev_polynomial_interpolation(n, a, b):
-    x = np.cos((2 * np.arange(1, n + 1) - 1) * np.pi / (2 * n))
-    y = FrankeFunction((b - a) / 2 * x + (a + b) / 2)
-    c = numpy.polynomial.chebyshev.chebfit(x, y, n - 1)
-    return numpy.polynomial.chebyshev.Chebyshev(c, domain=[a, b])
-
-n_chebyshev = 10
-chebyshev_poly = chebyshev_polynomial_interpolation(n_chebyshev, 0, 1)
-z_chebyshev = chebyshev_poly(x, y)
-chebroots = chebyshev_poly.roots()
-
-# Errores relativos para la función de Franke
-error_lagrange = np.abs((z - z_lagrange) / z)
-error_spline = np.abs((z - z_spline) / z)
-error_chebyshev = np.abs((z - z_chebyshev) / z)
-
-# Graficar para la función de Franke
-def graficos():
-    plt.figure(figsize=(12, 6))
-    #quiero hacer que los graficos tengan relacion 1:1, como hago eso?
-    # plt.axis('equal')
-
-    # Función original y Lagrage
-    plt.subplot(3, 2, 1)
-    plt.plot(x, y, z, label='fa(x)', color='blue')
-    plt.scatter(x_interpolation, y_interpolation, z_interpolation, label='Puntos de interpolación', color='black')
-    plt.plot(x, y, z_lagrange, label='Interpolación de Lagrange', linestyle='--', color='red')
-    plt.title('Comparación entre fa(x), Interpolación de Lagrange')
-    plt.legend()
-    plt.grid(True)
+# # Interpolación de Lagrange para la función de Franke
+# lagrange_poly = lagrange((x_interpolation, y_interpolation), z_interpolation)
+# z_lagrange = lagrange_poly((x, y))
+# # Polinomios cúbicos splines para la función de Franke
+# spline = CubicSpline((x_interpolation, y_interpolation), z_interpolation)
+# z_spline = spline((x, y))
 
 
-    # Error relativo de Lagrange para la función de Franke
-    plt.subplot(3, 2, 2)
-    plt.plot(x, y, error_lagrange, label='Error relativo de Lagrange', color='red')
-    plt.title('Error relativo de Lagrange')
-    plt.legend()
-    plt.grid(True)
+# # Polinomios de Chebyshev para la función de Franke
+# def chebyshev_polynomial_interpolation(n, a, b):
+#     x = np.cos((2 * np.arange(1, n + 1) - 1) * np.pi / (2 * n))
+#     y = FrankeFunction((b - a) / 2 * x + (a + b) / 2)
+#     c = numpy.polynomial.chebyshev.chebfit(x, y, n - 1)
+#     return numpy.polynomial.chebyshev.Chebyshev(c, domain=[a, b])
 
-    # Función original y Spline
-    plt.subplot(3, 2, 3)
-    plt.plot(x, y, z, label='fa(x)', color='blue')
-    plt.scatter(x_interpolation, y_interpolation, z_interpolation, label='Puntos de interpolación', color='black')
-    plt.plot(x, y, z_spline, label='Polinomio cúbico spline', linestyle=':', color='green')
+# n_chebyshev = 10
+# chebyshev_poly = chebyshev_polynomial_interpolation(n_chebyshev, 0, 1)
+# z_chebyshev = chebyshev_poly(x, y)
+# chebroots = chebyshev_poly.roots()
 
-    plt.title('Comparación entre fa(x) y Polinomi cúbico spline')
-    plt.xlabel('x', fontweight='bold', loc='right')
-    plt.ylabel('fa(x)', fontweight='bold', loc='top')
-    plt.legend()
-    plt.grid(True)
+# # Errores relativos para la función de Franke
+# error_lagrange = np.abs((z - z_lagrange) / z)
+# error_spline = np.abs((z - z_spline) / z)
+# error_chebyshev = np.abs((z - z_chebyshev) / z)
 
-    #Error relativo de Spline para la función de Franke
-    plt.subplot(3, 2, 4)
-    plt.title('Error relativo de Spline')
-    plt.plot(x, y, error_spline, label='Error relativo del polinomio cúbico spline', color='green')
-    plt.xlabel('x', fontweight='bold', loc='right')
-    plt.ylabel('Error relativo', fontweight='bold', loc='top')
-    plt.legend()
-    plt.grid(True)
+# # Graficar para la función de Franke
+# def graficos():
+#     plt.figure(figsize=(12, 6))
+#     #quiero hacer que los graficos tengan relacion 1:1, como hago eso?
+#     # plt.axis('equal')
 
-    # Función original y Chebyshev
-    plt.subplot(3, 2, 5)
-    plt.plot(x, y, z, label='fa(x)', color='blue')
-    plt.scatter(x_interpolation, y_interpolation, z_interpolation, label='Puntos de interpolación', color='black')
-    plt.plot(x, y, z_chebyshev, label='Polinomio de Chebyshev', linestyle='-.', color='purple')
-    plt.title('Comparación entre fa(x) y Polinomio de Chebyshev')
-    plt.xlabel('x', fontweight='bold', loc='right')
-    plt.ylabel('fa(x)', fontweight='bold', loc='top')
-    plt.legend()
-    plt.grid(True)
-
-    #Error relativo de Chebyshev para la función de Franke
-    plt.subplot(3, 2, 6)
-    plt.title('Error relativo de Chebyshev')
-    plt.plot(x, y, error_chebyshev, label='Error relativo del polinomio de Chebyshev', color='purple')   
-    plt.xlabel('x', fontweight='bold', loc='right')
-    plt.ylabel('Error relativo', fontweight='bold', loc='top')
-    plt.legend()
-    plt.grid(True)
+#     # Función original y Lagrage
+#     plt.subplot(3, 2, 1)
+#     plt.plot(x, y, z, label='fa(x)', color='blue')
+#     plt.scatter(x_interpolation, y_interpolation, z_interpolation, label='Puntos de interpolación', color='black')
+#     plt.plot(x, y, z_lagrange, label='Interpolación de Lagrange', linestyle='--', color='red')
+#     plt.title('Comparación entre fa(x), Interpolación de Lagrange')
+#     plt.legend()
+#     plt.grid(True)
 
 
-    plt.tight_layout()
-    plt.show()
+#     # Error relativo de Lagrange para la función de Franke
+#     plt.subplot(3, 2, 2)
+#     plt.plot(x, y, error_lagrange, label='Error relativo de Lagrange', color='red')
+#     plt.title('Error relativo de Lagrange')
+#     plt.legend()
+#     plt.grid(True)
 
-graficos()
+#     # Función original y Spline
+#     plt.subplot(3, 2, 3)
+#     plt.plot(x, y, z, label='fa(x)', color='blue')
+#     plt.scatter(x_interpolation, y_interpolation, z_interpolation, label='Puntos de interpolación', color='black')
+#     plt.plot(x, y, z_spline, label='Polinomio cúbico spline', linestyle=':', color='green')
+
+#     plt.title('Comparación entre fa(x) y Polinomi cúbico spline')
+#     plt.xlabel('x', fontweight='bold', loc='right')
+#     plt.ylabel('fa(x)', fontweight='bold', loc='top')
+#     plt.legend()
+#     plt.grid(True)
+
+#     #Error relativo de Spline para la función de Franke
+#     plt.subplot(3, 2, 4)
+#     plt.title('Error relativo de Spline')
+#     plt.plot(x, y, error_spline, label='Error relativo del polinomio cúbico spline', color='green')
+#     plt.xlabel('x', fontweight='bold', loc='right')
+#     plt.ylabel('Error relativo', fontweight='bold', loc='top')
+#     plt.legend()
+#     plt.grid(True)
+
+#     # Función original y Chebyshev
+#     plt.subplot(3, 2, 5)
+#     plt.plot(x, y, z, label='fa(x)', color='blue')
+#     plt.scatter(x_interpolation, y_interpolation, z_interpolation, label='Puntos de interpolación', color='black')
+#     plt.plot(x, y, z_chebyshev, label='Polinomio de Chebyshev', linestyle='-.', color='purple')
+#     plt.title('Comparación entre fa(x) y Polinomio de Chebyshev')
+#     plt.xlabel('x', fontweight='bold', loc='right')
+#     plt.ylabel('fa(x)', fontweight='bold', loc='top')
+#     plt.legend()
+#     plt.grid(True)
+
+#     #Error relativo de Chebyshev para la función de Franke
+#     plt.subplot(3, 2, 6)
+#     plt.title('Error relativo de Chebyshev')
+#     plt.plot(x, y, error_chebyshev, label='Error relativo del polinomio de Chebyshev', color='purple')   
+#     plt.xlabel('x', fontweight='bold', loc='right')
+#     plt.ylabel('Error relativo', fontweight='bold', loc='top')
+#     plt.legend()
+#     plt.grid(True)
+
+
+#     plt.tight_layout()
+#     plt.show()
+
+# graficos()
+
--- 2.py
+++ 2.py
@@ -1,3 +1,44 @@
-with open("/Users/franco/Documentos/UdeSA/2do año/1er cuatrimestre/Metodos numericos/MNyO_TP01_2024_1S/mnyo_mediciones.csv", 'r') as file:
-    data = file.read()
-    print(data)
+'''Se ha registrado la posición de un vehículo autónomo que circula en un parque industrial mediante GPS
+en distintos instantes de tiempo (ver archivo mediciones.csv). La posición del vehículo se registra en dos
+dimensiones en coordenadas x1(ti), x2(ti).
+• Utilizando los datos provistos (mediciones.csv) recupere la trayectoria del vehículo interpolando las
+posiciones provistas y compárelas con la trayectoria real (ground truth provisto en groundtruth.csv).'''
+
+# dado mnyo_mediciones.csv, se observa que el archivo tiene una sola columna con los valores de x1 y x2 separados por un espacio.
+# se puede leer el archivo con pandas y luego separar los valores de x1 y x2 en dos columnas distintas.
+# luego se puede interpolar los valores de x1 y x2 por separado y compararlos con los valores de ground truth.
+# aclaracion: no tiene headers x1 y x2, por lo que se debe especificar que no tiene headers al leer el archivo.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import lagrange, CubicSpline
+
+# 1. Leer los datos
+# Leer mediciones.csv
+df = pd.read_csv('mnyo_mediciones.csv', header=None, names=['x'])
+# Separar los valores de x1 y x2 en dos columnas distintas
+df = df['x'].str.split(' ', expand=True)
+df.columns = ['x1', 'x2']
+# Convertir a float
+df = df.astype(float)
+
+# Leer groundtruth.csv
+df_gt = pd.read_csv('mnyo_ground_truth.csv', header=None, names=['x'])
+# Separar los valores de x1 y x2 en dos columnas distintas
+df_gt = df_gt['x'].str.split(' ', expand=True)
+df_gt.columns = ['x1', 'x2']
+# Convertir a float
+df_gt = df_gt.astype(float)
+
+print(df_gt)
+
+# 3. Interpolación
+# Interpolación de Lagrange
+t = np.arange(len(df))
+t_gt = np.arange(len(df_gt))
+x1 = df['x1'].values
+x2 = df['x2'].values
+x1_gt = df_gt['x1'].values
+x2_gt = df_gt['x2'].values
+
