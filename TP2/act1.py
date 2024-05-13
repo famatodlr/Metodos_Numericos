d5830b09590a4d99c43f9df5d92f024e1472bf5f
totobena28 <benavideztomas28@gmail.com>
2024-05-13 18:13:02 -0300

    .

--- TP2/act1.py
+++ TP2/act1.py
@@ -1,80 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# # Valores iniciales
-# datos_iniciales = {
-#     'N0': 25,
-#     'r': 0.05,
-#     'K': 500
-# }
-
-# # Pendiente exponencial
-# def pendiente_exp(t, n):
-#     return datos_iniciales['r'] * n
-
-# # Pendiente logística
-# def pendiente_log(t, n):
-#     return datos_iniciales['r'] * n * (1 - n/ datos_iniciales['K'])
-
-# # Gráfica
-# def grafico1():
-#     N = np.linspace(0, 500, 100)
-
-#     plt.plot(N, pendiente_exp(0, N), label='Exponencial')
-#     plt.plot(N, pendiente_log(0, N), label='Logística')
-#     plt.xlabel('Población')
-#     plt.ylabel('Tasa de crecimiento')
-#     plt.title('Pendiente de crecimiento')
-#     # plt.ylim(0, 100)
-#     plt.legend()
-#     plt.plot([500, 500], [0, 25], 'k--')
-#     plt.plot([0, 500], [25, 25], 'k--')
-#     plt.plot([500], [25], 'ro')
-#     plt.scatter([500], [25], color='red')
-#     plt.show()
-
-# def f_exp(t, n0, r):
-#     return n0 * np.exp((r * t))
-
-# def f_log(t, n0, K, r):
-    # https://courses.lumenlearning.com/calculus2/chapter/solving-the-logistic-differential-equation/
-
-#     # return K / (1 + (K/n0 - 1) * np.exp(-r*t))
-#     return n0 * K * np.exp(r * t)/ ((K - n0) + n0 * np.exp(-r * t))  
-
-# #Grafico de f_exp
-# def grafico2():
-#     y_exp = f_exp(t, datos_iniciales['N0'], datos_iniciales['r'])
-#     plt.plot(t, y_exp)
-#     plt.xlabel('Tiempo')
-#     plt.ylabel('Poblacion')
-#     plt.title('Modelo Exponencial')
-
-#     plt.annotate(f'N0 = {datos_iniciales["N0"]}\nr = {datos_iniciales["r"]}', xy=(0, 1), xycoords='axes fraction', fontsize=12,
-#                 xytext=(10, -10), textcoords='offset points',
-#                 ha='left', va='top', bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5))
-
-#     plt.show()
-
-# #Grafico de f_log
-# def grafico3():
-#     y_log = f_log(t, datos_iniciales['N0'], datos_iniciales['K'], datos_iniciales['r'])
-#     plt.plot(t, y_log)
-#     plt.xlabel('Tiempo')
-#     plt.plot([45.8, 45.8], [0, 500], 'k--')
-#     plt.plot([0, 45.8], [500, 500], 'k--')
-#     plt.plot([45.8], [500], 'ro')
-#     plt.ylabel('Poblacion')
-#     plt.title('Modelo Logistico')
-
-#     plt.annotate(f'N0 = {datos_iniciales["N0"]}\nK = {datos_iniciales["K"]}\nr = {datos_iniciales["r"]}', xy=(0, 1), xycoords='axes fraction', fontsize=12,
-#                 xytext=(10, -10), textcoords='offset points',
-#                 ha='left', va='top', bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5))
-
-#     plt.show()
-
-
-
 def variacion_poblacional_exponencial(r, N):
     return r * N
 
@@ -114,7 +40,6 @@ def caso2():
     plt.legend()
     plt.show()
 
-
 def caso3():
     t = np.linspace(0, 100, 100)
     N0 = 350
@@ -149,6 +74,7 @@ def caso1VN():
     plt.show()
 
 def caso2VN():
+    
     N = np.linspace(0, 1000, 100)
     r = 0.05
     K = 1000
@@ -172,103 +98,3 @@ def caso2VN():
     plt.ylabel('Variación poblacional')
     plt.legend()
     plt.show()
-
-
-
-def runge_kutta_log(f, x0, y0, h, n, r = 0.05, K = 500):
-    x = x0
-    y = y0
-    for _ in range(n):
-        k1 = h * f(x, y, K, r)
-        k2 = h * f(x + h/2, y + k1/2, K, r)
-        k3 = h * f(x + h/2, y + k2/2, K, r)
-        k4 = h * f(x + h, y + k3, K, r)
-        y = y + (k1 + 2*k2 + 2*k3 + k4)/6
-        x = x + h
-    return y
-
-def runge_kutta_exp(f, x0, y0, h, n, r = 0.05):
-    x = x0
-    y = y0
-    for _ in range(n):
-        k1 = h * f(r, y)
-        k2 = h * f(r, y + k1/2)
-        k3 = h * f(r, y + k2/2)
-        k4 = h * f(r, y + k3)
-        y = y + (k1 + 2*k2 + 2*k3 + k4)/6
-        x = x + h
-    return y
-
-def euler_log(f, x0, y0, h, n, r = 0.05, K = 500):
-    x = x0
-    y = y0
-    for _ in range(n):
-        y = y + h * f(x, y, K, r)
-        x = x + h
-    return y
-
-def euler_exp(f, x0, y0, h, n, r = 0.05):
-    x = x0
-    y = y0
-    for _ in range(n):
-        y = y + h * f(r, y)
-        x = x + h
-    return y
-
-
-def error_absoluto(y, y_hat):
-    return abs(y - y_hat)
-
-
-# Error en Runge-Kutta y Euler funcion exponencial
-def grafico_errores_exp():
-    h = 0.1
-    N0 = 25
-    r = 0.05
-    t = np.linspace(0, 500, 500)
-    n = 500
-
-    y = solucion_exponencial(t, N0, r)
-
-    y_rk = np.array([runge_kutta_exp(variacion_poblacional_exponencial, 0, N0, h, i) for i in range(n)])
-    y_euler = np.array([euler_exp(variacion_poblacional_exponencial, 0, N0, h, i) for i in range(n)])
-
-    error_rk = error_absoluto(y, y_rk)
-    error_euler = error_absoluto(y, y_euler)
-
-    plt.plot(t, error_rk, label='Runge-Kutta')
-    plt.plot(t, error_euler, label='Euler')
-    plt.xlabel('Tiempo')
-    plt.ylabel('Error')
-    # plt.yscale('log')
-    plt.legend()
-    plt.title('Error en Runge-Kutta y Euler (Modelo Exponencial)')
-    plt.show()
-
-def grafico_errores_log():
-
-    h = 0.1
-    N0 = 25
-    r = 0.05
-    t = np.linspace(0, 500, 500)
-    n = 500
-
-    # Error en Runge-Kutta y Euler funcion logistica
-    y = solucion_logistica(t, N0, 500, r)
-    y_rk = np.array([runge_kutta_log(solucion_logistica, 0, N0, h, i) for i in range(n)])
-    y_euler = np.array([euler_log(solucion_logistica, 0, N0, h, i) for i in range(n)])
-
-    error_rk = error_absoluto(y, y_rk)
-    error_euler = error_absoluto(y, y_euler)
-
-    plt.plot(t, error_rk, label='Runge-Kutta')
-    plt.plot(t, error_euler, label='Euler')
-    plt.xlabel('Tiempo')
-    plt.ylabel('Error')
-    plt.yscale('log')
-    plt.legend()
-    plt.title('Error en Runge-Kutta y Euler (Modelo Logistico)')
-    plt.show()
-
-grafico_errores_exp()
-grafico_errores_log()
