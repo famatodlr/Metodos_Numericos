86eeb12f974bb8bc4320a5274691fabc64dadb4a
totobena28 <benavideztomas28@gmail.com>
2024-05-12 22:30:29 -0300

    actualizacion de graficos act1

--- TP2/act1.py
+++ TP2/act1.py
@@ -73,67 +73,7 @@ import matplotlib.pyplot as plt
 
 #     plt.show()
 
-# def runge_kutta(f, x0, y0, h, n):
-#     x = x0
-#     y = y0
-#     for _ in range(n):
-#         k1 = h * f(x, y)
-#         k2 = h * f(x + h/2, y + k1/2)
-#         k3 = h * f(x + h/2, y + k2/2)
-#         k4 = h * f(x + h, y + k3)
-#         y = y + (k1 + 2*k2 + 2*k3 + k4)/6
-#         x = x + h
-#     return y
 
-# def euler(f, x0, y0, h, n):
-#     x = x0
-#     y = y0
-#     for _ in range(n):
-#         y = y + h * f(x, y)
-#         x = x + h
-#     return y
-
-# def error_absoluto(y, y_hat):
-#     return abs(y - y_hat)
-
-
-# # Error en Runge-Kutta y Euler funcion exponencial
-# h = 0.1
-# n = 100
-# t = np.linspace(0, 100, n)
-
-# y = f_exp(t, datos_iniciales['N0'], datos_iniciales['r'])
-# y_rk = np.array([runge_kutta(pendiente_exp, 0, datos_iniciales['N0'], h, i) for i in range(n)])
-# y_euler = np.array([euler(pendiente_exp, 0,datos_iniciales['N0'], h, i) for i in range(n)])
-
-# error_rk = error_absoluto(y, y_rk)
-# error_euler = error_absoluto(y, y_euler)
-
-# plt.plot(t, error_rk, label='Runge-Kutta')
-# plt.plot(t, error_euler, label='Euler')
-# plt.xlabel('Tiempo')
-# plt.ylabel('Error')
-# plt.yscale('log')
-# plt.legend()
-# plt.title('Error en Runge-Kutta y Euler (Modelo Exponencial)')
-# plt.show()
-
-# # Error en Runge-Kutta y Euler funcion logistica
-# y = f_log(t, datos_iniciales['N0'], datos_iniciales['K'], datos_iniciales['r'])
-# y_rk = np.array([runge_kutta(pendiente_log, 0, datos_iniciales['N0'], h, i) for i in range(n)])
-# y_euler = np.array([euler(pendiente_log, 0, datos_iniciales['N0'], h, i) for i in range(n)])
-
-# error_rk = error_absoluto(y, y_rk)
-# error_euler = error_absoluto(y, y_euler)
-
-# plt.plot(t, error_rk, label='Runge-Kutta')
-# plt.plot(t, error_euler, label='Euler')
-# plt.xlabel('Tiempo')
-# plt.ylabel('Error')
-# plt.yscale('log')
-# plt.legend()
-# plt.title('Error en Runge-Kutta y Euler (Modelo Logistico)')
-# plt.show()
 
 def variacion_poblacional_exponencial(r, N):
     return r * N
@@ -233,3 +173,102 @@ def caso2VN():
     plt.legend()
     plt.show()
 
+
+
+def runge_kutta_log(f, x0, y0, h, n, r = 0.05, K = 500):
+    x = x0
+    y = y0
+    for _ in range(n):
+        k1 = h * f(x, y, K, r)
+        k2 = h * f(x + h/2, y + k1/2, K, r)
+        k3 = h * f(x + h/2, y + k2/2, K, r)
+        k4 = h * f(x + h, y + k3, K, r)
+        y = y + (k1 + 2*k2 + 2*k3 + k4)/6
+        x = x + h
+    return y
+
+def runge_kutta_exp(f, x0, y0, h, n, r = 0.05):
+    x = x0
+    y = y0
+    for _ in range(n):
+        k1 = h * f(r, y)
+        k2 = h * f(r, y + k1/2)
+        k3 = h * f(r, y + k2/2)
+        k4 = h * f(r, y + k3)
+        y = y + (k1 + 2*k2 + 2*k3 + k4)/6
+        x = x + h
+    return y
+
+def euler_log(f, x0, y0, h, n, r = 0.05, K = 500):
+    x = x0
+    y = y0
+    for _ in range(n):
+        y = y + h * f(x, y, K, r)
+        x = x + h
+    return y
+
+def euler_exp(f, x0, y0, h, n, r = 0.05):
+    x = x0
+    y = y0
+    for _ in range(n):
+        y = y + h * f(r, y)
+        x = x + h
+    return y
+
+
+def error_absoluto(y, y_hat):
+    return abs(y - y_hat)
+
+
+# Error en Runge-Kutta y Euler funcion exponencial
+def grafico_errores_exp():
+    h = 0.1
+    N0 = 25
+    r = 0.05
+    t = np.linspace(0, 500, 500)
+    n = 500
+
+    y = solucion_exponencial(t, N0, r)
+
+    y_rk = np.array([runge_kutta_exp(variacion_poblacional_exponencial, 0, N0, h, i) for i in range(n)])
+    y_euler = np.array([euler_exp(variacion_poblacional_exponencial, 0, N0, h, i) for i in range(n)])
+
+    error_rk = error_absoluto(y, y_rk)
+    error_euler = error_absoluto(y, y_euler)
+
+    plt.plot(t, error_rk, label='Runge-Kutta')
+    plt.plot(t, error_euler, label='Euler')
+    plt.xlabel('Tiempo')
+    plt.ylabel('Error')
+    # plt.yscale('log')
+    plt.legend()
+    plt.title('Error en Runge-Kutta y Euler (Modelo Exponencial)')
+    plt.show()
+
+def grafico_errores_log():
+
+    h = 0.1
+    N0 = 25
+    r = 0.05
+    t = np.linspace(0, 500, 500)
+    n = 500
+
+    # Error en Runge-Kutta y Euler funcion logistica
+    y = solucion_logistica(t, N0, 500, r)
+    y_rk = np.array([runge_kutta_log(solucion_logistica, 0, N0, h, i) for i in range(n)])
+    y_euler = np.array([euler_log(solucion_logistica, 0, N0, h, i) for i in range(n)])
+
+    error_rk = error_absoluto(y, y_rk)
+    error_euler = error_absoluto(y, y_euler)
+
+    plt.plot(t, error_rk, label='Runge-Kutta')
+    plt.plot(t, error_euler, label='Euler')
+    plt.xlabel('Tiempo')
+    plt.ylabel('Error')
+    plt.yscale('log')
+    plt.legend()
+    plt.title('Error en Runge-Kutta y Euler (Modelo Logistico)')
+    plt.show()
+
+grafico_errores_exp()
+grafico_errores_log()
